6a068427d03f5282b4b654fa0ee81bddf5ee09db
DESKTOP-FOFAO14\MOON <moonyoungsu@naver.com>
2021-09-05 01:23:30 +0900

    updata SpringDataJpa
    스프링 데이터 JPA를 사용하여 코드 단순화

--- hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -13,25 +13,25 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-  private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    @Autowired //생성자가 한개인 경우는 @Autowired 생략가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
 
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-
-        //return new MemoryMemberRepository();
-        //return new JdbcMemberRepository(dataSource);
+//    @Bean
+//    public MemberRepository memberRepository() {
+//
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
